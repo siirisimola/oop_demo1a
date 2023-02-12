b6fe81076870c46a20eea5c908bdf07819b8d1dd
siirisimola <94476037+siirisimola@users.noreply.github.com>
2023-02-13 01:59:46 +0200

    kaipaa ehkä vielä hiomista, mutta rakenne ehkä jo suht ok

--- Esiintyjat.java
+++ Esiintyjat.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 public class Esiintyjat {
     //esiintyjän tiedot tänne
     private String nimi;
     private String puhnro;
     private String ala;
     private double palkkio;
+    ArrayList<String> esiintyjat = new ArrayList<>();
 }
--- Varausjarjestelma.java
+++ Varausjarjestelma.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
 public class Varausjarjestelma {
+    private ArrayList<Asiakas> asiakkaat;
+    private ArrayList<Esiintyjat> esiintyjat;
+    private Juhla juhla;
+    private ArrayList<Varaus> varaukset;
+
+    public ArrayList<Asiakas> getAsiakas() {
+        return asiakkaat;
+    }
+
+    public ArrayList<Esiintyjat> getEsiintyjat() {
+        return esiintyjat;
+    }
+
+    public Juhla getJuhla() {
+        return juhla;
+    }
+
+    public ArrayList<Varaus> getVaraus() {
+        return varaukset;
+    }
+
+    public Varausjarjestelma(ArrayList<Asiakas> asiakkaat, ArrayList<Esiintyjat> esiintyjat, Juhla juhla, ArrayList<Varaus> varaukset) {
+        this.asiakkaat = asiakkaat;
+        this.esiintyjat = esiintyjat;
+        this.juhla = juhla;
+        this.varaukset = varaukset;
+    }
 }
